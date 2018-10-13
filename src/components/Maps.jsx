bd5e6c9013dffede67fe208e1541b87c8b3c7d5b
AndrewKim <kdk0063@gmail.com>
2018-10-13 18:43:40 -0400

    wip

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,17 +1,34 @@
-import React, { Component} from 'react';
-
-class Maps extends Component {
-
-    render() {
-        return (      
-            <div className="container shadow-the-back-sm">  
-                <div className="row bg-light mb-4">
-                    <div className="col-lg-6 col-12 map">
-                        <div style={{ width: "100%" }}><iframe width="100%" height="350px" src="https://www.mapsdirections.info/en/custom-google-maps/map.php?width=100%&height=600&hl=ru&q=301%20Westwood%20Plaza%20Los%20Angeles%2C%20CA%2090095+(Interview%20Me)&ie=UTF8&t=&z=14&iwloc=B&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" title='google' className="shadow-the-back-sm"><a href="https://www.mapsdirections.info/en/custom-google-maps/">Create Google Map</a> by <a href="https://www.mapsdirections.info/en/">Measure area on map</a></iframe></div>
-                    </div>
-                </div>
-            </div> 
-        )
-    }
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class Gmaps extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyCGM--LTOYnIFVX4HZsZbg6bIT5P1bBNO0" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
-export default Maps;
+ 
+export default Gmaps;
