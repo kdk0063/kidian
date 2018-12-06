4d41f1bc9e0cd6f2f485e8d34da199807175c9ea
AndrewKim <kdk0063@gmail.com>
2018-12-06 14:42:43 -0500

    WIP

--- src/components/DetailInfo.jsx
+++ src/components/DetailInfo.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import '../css/DetailInfo.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import isAfter from 'date-fns/isAfter';
 
 class DetailInfo extends Component{
 
